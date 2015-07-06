40a65e340ac3b2ec15f85070b587182fc31ef06c
allen-smithee <allen-smithee@users.noreply.github.com>
2015-07-07 00:28:29 +0100

    Add sample mobile app

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # graffiti
+
+# Getting started
+
+Install Xcode.
+
+Install Brew: ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+Install Git: brew install git
+
+Install Node: brew install node
+
+Install Cordova: sudo npm install -g cordova
+
+Clone Repo: git clone https://github.com/allen-smithee/graffiti.git
+
+Run: cd graffiti/mobile-app-sample; cordova run ios
+
+Debug: Debug -> Location -> ...
+
---
+++ mobile-app-sample/platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,54 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.transistorsoft.cordova.background-geolocation/www/BackgroundGeoLocation.js",
+        "id": "org.transistorsoft.cordova.background-geolocation.BackgroundGeoLocation",
+        "clobbers": [
+            "plugins.backgroundGeoLocation"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/Coordinates.js",
+        "id": "cordova-plugin-geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+        "id": "cordova-plugin-geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/Position.js",
+        "id": "cordova-plugin-geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/geolocation.js",
+        "id": "cordova-plugin-geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.transistorsoft.cordova.background-geolocation": "0.3.6",
+    "cordova-plugin-geolocation": "1.0.1",
+    "cordova-plugin-dialogs": "1.1.1"
+}
+// BOTTOM OF METADATA
+});
